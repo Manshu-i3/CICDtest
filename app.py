3cd5b5a52fff042bc571021e9abb0bd2fe78d059
Manshu-i3 <manshu.jaiswar@tudip.com>
2024-12-18 18:55:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Manshu Jaiswar, This is Flask app running on 5000 port!"
+    return "Hello Manshu Jaiswar, This is Flask app running on port 5000!"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
