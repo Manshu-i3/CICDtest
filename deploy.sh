c4fcd8f6b83ad3f488cb226b35679c527191bf0a
Manshu-i3 <manshu.jaiswar@tudip.com>
2024-12-18 16:48:31 +0530

    Update deploy.sh@16:48

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,20 @@
 PARAMETER_NAME="/flask/sshkey"
 EC2_USER="ubuntu"
 EC2_HOST="ec2-13-233-153-140.ap-south-1.compute.amazonaws.com"  # Replace with your EC2 instance's public IP
-SSH_KEY_PATH="/home/ubuntu/my-ssh-key.pem"
+SSH_KEY_PATH="/my-ssh-key.pem"
 
 # Retrieve the SSH key from AWS Systems Manager Parameter Store
 echo "Retrieving SSH key from SSM"
 aws ssm get-parameter --name "$PARAMETER_NAME" --with-decryption --query "Parameter.Value" --output text > $SSH_KEY_PATH
 
 # Change permissions for the SSH private key
-
+  chmod 400 $SSH_KEY_PATH
 
 # SSH into the EC2 instance and perform deployment tasks
 echo "Deploying to EC2 instance..."
 ssh -i $SSH_KEY_PATH $EC2_USER@$EC2_HOST << EOF
   # Navigate to the app directory on EC2
-  chmod 400 $SSH_KEY_PATH
+ 
   cd /home/ubuntu/Deployment || exit
   git pull https://github.com/Manshu-i3/CICDtest.git 
   python3 app.py 
