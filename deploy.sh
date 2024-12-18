0b0a721e5a7b6b8506ffe4c6bb3f4549dab76fb6
Manshu-i3 <manshu.jaiswar@tudip.com>
2024-12-18 16:46:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,16 @@ echo "Retrieving SSH key from SSM"
 aws ssm get-parameter --name "$PARAMETER_NAME" --with-decryption --query "Parameter.Value" --output text > $SSH_KEY_PATH
 
 # Change permissions for the SSH private key
-chmod 400 $SSH_KEY_PATH
+
 
 # SSH into the EC2 instance and perform deployment tasks
 echo "Deploying to EC2 instance..."
 ssh -i $SSH_KEY_PATH $EC2_USER@$EC2_HOST << EOF
   # Navigate to the app directory on EC2
+  chmod 400 $SSH_KEY_PATH
   cd /home/ubuntu/Deployment || exit
-     git pull https://github.com/Manshu-i3/CICDtest.git 
-     python3 app.py 
+  git pull https://github.com/Manshu-i3/CICDtest.git 
+  python3 app.py 
 EOF
 
 echo "Deployment completed successfully!"
